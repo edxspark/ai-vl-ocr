69d6980411bc358e72d847af23f14aaf6d47b71a
moks.mo <moks.mo@edxspark.com>
2024-11-25 22:41:38 +0800

    fix import bug.

--- src/App.py
+++ src/App.py
@@ -1,12 +1,10 @@
 #!/usr/bin/venv python
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.agent import AIOCRAgent
 from src.domain.BO import AIVLBo
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 
