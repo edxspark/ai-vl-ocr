7166a098100990f9fbb3a1630ef4bdb6bdad4065
moks.mo <moks.mo@edxspark.com>
2024-11-29 01:47:31 +0800

    update api host.

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'https://u260419-b6e5-4f370630.bjb1.seetacloud.com:8443', // 目标服务器地址
-        changeOrigin: true, // 改变请求的origin
-        pathRewrite: { '^/api': '' } // 重写请求路径
+        target: 'https://u260419-a5e7-81a835e7.bjb1.seetacloud.com:8443',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
       }
     }
   }
