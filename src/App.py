d6311526a2968f0bd2eafb146e620762a8b0b340
moks.mo <moks.mo@edxspark.com>
2024-12-04 03:48:14 +0800

    Update modelscope.

--- src/App.py
+++ src/App.py
@@ -1,11 +1,10 @@
 #!/usr/bin/venv python
 import os
 import sys
-
-from src.llm.QwenVL import QwenVL
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
+from src.llm.QwenVL import QwenVL
 from src.agent import AIOCRAgent
 from src.domain.BO import AIVLBo
 from fastapi import FastAPI, UploadFile, File, Form
