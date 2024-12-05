acae794397d1d1e306e280a7e3fc4c32e5fc4519
moks.mo <moks.mo@edxspark.com>
2024-12-06 01:18:14 +0800

    update .env and readme.

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: {
-      '/api': {
-        target: process.env.VUE_APP_BACKEND_HOST,
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: process.env.VUE_APP_BACKEND_HOST,
+    //     changeOrigin: true,
+    //     pathRewrite: { '^/api': '' }
+    //   }
+    // }
   }
 })
